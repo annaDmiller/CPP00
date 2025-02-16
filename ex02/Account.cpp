ef600d16c702f0c83e6cfd872f4bbd43b6219b13
Anna Melnikova <amelniko@student.42.fr>
2025-02-16 23:09:19 +0100

    Start of recovering the functions' code; commenting some part of header and main to run unit tests one by one

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -10,4 +10,55 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Account.hpp"
+#include <ctime>
+#include <string>
+#include <iostream>
+#include <iomanip>
 
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+    : _accountIndex(_nbAccounts), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)
+{
+    Account::_displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";";
+    std::cout << "amount:" << _amount << ";";
+    std::cout << "created" << std::endl;
+    _nbAccounts++;
+    _totalAmount += initial_deposit;
+    return ;
+}
+
+Account::Account(void)
+{
+    return ;
+}
+
+Account::~Account(void)
+{
+    return ;
+}
+
+void	Account::displayAccountsInfos( void )
+{
+    Account::_displayTimestamp();
+    std::cout << "accounts:" << Account::_nbAccounts << ";";
+    std::cout << "total:" << Account::_totalAmount << ";";
+    std::cout << "deposits:" << Account::_totalNbDeposits << ";";
+    std::cout << "withdrawals:" << Account::_totalNbWithdrawals << std::endl;
+    return ;
+}
+
+void	Account::_displayTimestamp( void )
+{
+    std::time_t now = std::time(nullptr);
+    std::tm* timeInfo = std::localtime(&now);
+    std::ostringstream oss;
+    oss << std::put_time(timeInfo, "%Y%m%d_%H%M%S");
+    std::cout << "[" << oss.str() << "] ";
+    return ;
+}
